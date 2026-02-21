035ee292ad7c02653cc262f49cb7eabb8f338db1
Imranullah Khan <imranullahkhann2004@gmail.com>
2026-02-21 19:30:37 +0500

    Add v1 and v2

---
+++ 1/init.vim
@@ -0,0 +1,8 @@
+set number
+set relativenumber
+set mouse=
+set tabstop=4
+set shiftwidth=4
+set clipboard=unnamedplus
+set wrap
+colorscheme retrobox
---
+++ 2/nvim/lua/lsp.lua
@@ -0,0 +1,40 @@
+vim.lsp.enable("codebook")
+vim.lsp.enable("gopls")
+
+-- LSP Configuration: This autocommand triggers whenever a Language Server attaches to a buffer.
+-- It maps essential IDE features (Definition, Hover, Rename, etc.) as buffer-local shortcuts,
+-- ensuring these keys only perform LSP actions when a server is active. It also includes 
+-- consistent keybindings for navigating and expanding diagnostic errors and warnings.
+vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+    callback = function(ev)
+        local opts = { buffer = ev.buf }
+        local set = vim.keymap.set
+
+        -- Hover documentation
+        set('n', 'K', vim.lsp.buf.hover, opts)
+        -- Go to definition
+        set('n', 'gd', vim.lsp.buf.definition, opts)
+        -- Go to declaration
+        set('n', 'gD', vim.lsp.buf.declaration, opts)
+        -- Go to implementation
+        set('n', 'gi', vim.lsp.buf.implementation, opts)
+        -- Go to type definition
+        set('n', 'gt', vim.lsp.buf.type_definition, opts)
+        -- Rename symbol
+        set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+        -- Code actions
+        set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
+        -- References
+        set('n', 'gr', vim.lsp.buf.references, opts)
+        -- Format buffer
+        set('n', '<leader>f', function()
+            vim.lsp.buf.format { async = true }
+        end, opts)
+
+        -- Diagnostic Keybindings
+        set('n', '[d', vim.diagnostic.goto_prev, opts)
+        set('n', ']d', vim.diagnostic.goto_next, opts)
+        set('n', '<leader>e', vim.diagnostic.open_float, opts)
+    end,
+})
